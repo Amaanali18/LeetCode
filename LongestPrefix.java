03f313bd586d2084a7b5a81b785834c2dc8b30d6
Amaanali <81069488+Amaanali18@users.noreply.github.com>
2024-09-25 11:14:43 +0530

    longest common prefix of a string

--- LongestPrefix.java
+++ LongestPrefix.java
@@ -1,20 +1,23 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
+        // if null str return empty
         if(strs==null||strs.length==0){
             return "";
         }
         String temp=strs[0];
         for(int i=1;i<strs.length;i++){
             int j=0;
+            // compare temp variable to any one of str
             while(j<temp.length()&&j<strs[i].length()){
                 if(temp.charAt(j)!=strs[i].charAt(j)){
-                    break;
+                    break;//if not equal break
                 }
                 j++;
             }
+            // add the found char to temp
             temp=temp.substring(0,j);
             if(temp.isEmpty()){
-                return "";
+                return "";// fast return if nothing is found
             }
         }
         return temp;
